4b0254f060073f3bb64e484035a13c9e5587758f
vagrant <akzouksia@icloud.com>
2020-11-04 17:03:50 +0900

    [Add]Home_controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 	  passwords:     'customers/passwords',
 	  registrations: 'customers/registrations'
 	}
+
+	get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
