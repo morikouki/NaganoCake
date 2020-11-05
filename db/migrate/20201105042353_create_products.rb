ca009b7a62ea801c3367a46acefe21229086b59a
vagrant <akzouksia@icloud.com>
2020-11-05 13:25:44 +0900

    [Add]Genre_model

--- db/migrate/20201105042353_create_products.rb
+++ db/migrate/20201105042353_create_products.rb
@@ -1,6 +1,6 @@
-class CreateUserProducts < ActiveRecord::Migration[5.2]
+class CreateProducts < ActiveRecord::Migration[5.2]
   def change
-    create_table :user_products do |t|
+    create_table :products do |t|
       t.integer :genre_id
       t.string :name
       t.string :image_id
