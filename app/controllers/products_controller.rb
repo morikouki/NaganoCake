a8d9abd48b39faab36d99b404522155f4cc2c059
vagrant <akzouksia@icloud.com>
2020-11-05 14:08:57 +0900

    [Add]gem 'refile'

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+
+	def new
+		@product = Product.new
+	end
+
+	def index
+		@products = Product.all
+	end
+
+	def create
+		product = Product.new(product_params)
+		genre = Genre.find(prams[:genre_id])
+		product.genre_id = genre.id
+		product.save
+		redirect_to products_path
+	end
+
+	private
+	def product_params
+		params.require(:product).permit(:name, :image, :explain, :status, :price)
+	end
 end
+
