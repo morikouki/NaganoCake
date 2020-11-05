f0faee2ebfac3534ccde063fc8e3d697f1b7611a
vagrant <akzouksia@icloud.com>
2020-11-05 14:36:28 +0900

    [Add]product_show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,6 +14,11 @@ class ProductsController < ApplicationController
 		redirect_to products_path
 	end
 
+	def show
+		@product = Product.find(params[:id])
+	end
+
+
 	private
 	def product_params
 		params.require(:product).permit(:name, :image, :explain, :status, :price, :genre_id)
