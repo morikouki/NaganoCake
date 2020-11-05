15807a6a10c978a6dbb1f78ea68de20e4a6098c5
vagrant <akzouksia@icloud.com>
2020-11-05 14:34:07 +0900

    [Update]product new,index_view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,15 +10,13 @@ class ProductsController < ApplicationController
 
 	def create
 		product = Product.new(product_params)
-		genre = Genre.find(prams[:genre_id])
-		product.genre_id = genre.id
 		product.save
 		redirect_to products_path
 	end
 
 	private
 	def product_params
-		params.require(:product).permit(:name, :image, :explain, :status, :price)
+		params.require(:product).permit(:name, :image, :explain, :status, :price, :genre_id)
 	end
 end
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ac44060fd9335d72d111aaa56168890fff7b32d47ef2b207f3af98bf551c8fb8d6fcf1caf720cfd1520761ad9148bf2fbbe2006c4369a4141291120d43d411ed'
