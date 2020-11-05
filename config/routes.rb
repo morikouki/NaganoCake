361fd27ccd7787ad87cbec9d9ced5885aabdbd0a
vagrant <akzouksia@icloud.com>
2020-11-05 14:02:11 +0900

    [Add]product new,index_view

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
 	get 'about' => 'home#about'
 
 	resources :genres, only: [:index, :create, :edit, :update]
+	resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
