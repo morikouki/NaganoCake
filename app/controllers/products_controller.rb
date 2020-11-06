83080aa4d70b9edabfd74343f28164fc9bca06c6
vagrant <akzouksia@icloud.com>
2020-11-06 14:25:41 +0900

    [Add]product edit_view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,17 @@ class ProductsController < ApplicationController
 		@product = Product.find(params[:id])
 	end
 
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		product = Product.find(params[:id])
+		product.update(product_params)
+		redirect_to products_path
+	end
+
+
 
 	private
 	def product_params
