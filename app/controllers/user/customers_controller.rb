5e1e68ccfba55653f20d25ce514e4ea18c025e02
vagrant <akzouksia@icloud.com>
2020-11-07 20:52:24 +0900

    [Update]customer signed_up

--- app/controllers/user/customers_controller.rb
+++ app/controllers/user/customers_controller.rb
@@ -1,2 +1,6 @@
 class User::CustomersController < ApplicationController
+
+	private
+	def customer_params
+		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :phone_number, :address, :is_deleted)
 end
