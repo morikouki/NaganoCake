35251d74ca2841aad526fd7b7185e07c540fdbd8
vagrant <akzouksia@icloud.com>
2020-11-08 13:11:45 +0900

    [Add]customer edit_view

--- app/controllers/user/customers_controller.rb
+++ app/controllers/user/customers_controller.rb
@@ -5,6 +5,19 @@ class User::CustomersController < ApplicationController
 		@customer = current_customer
 	end
 
+	def edit
+		@customer = current_customer
+	end
+
+	def update
+	end
+
+	def confirm
+	end
+
+	def withdraw
+	end
+
 	private
 	def customer_params
 		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :phone_number, :address, :is_deleted)
