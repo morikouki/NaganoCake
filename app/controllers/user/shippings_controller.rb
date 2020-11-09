2a4190dc7ac8e09bcb22c1bee1a35a65f6daf833
vagrant <akzouksia@icloud.com>
2020-11-09 14:47:58 +0900

    [Update]shippings index_view

--- app/controllers/user/shippings_controller.rb
+++ app/controllers/user/shippings_controller.rb
@@ -1,7 +1,21 @@
 class User::ShippingsController < ApplicationController
 
 	def index
+		@shipping = Shipping.new
+		@shippings = Shipping.all
 	end
+
+	def create
+		shipping = current_customer.shippings.new(shipping_params)
+		shipping.save
+		redirect_to user_shippings_path
+	end
+
+
+
+
+
+
 
 	private
 	def shipping_params
