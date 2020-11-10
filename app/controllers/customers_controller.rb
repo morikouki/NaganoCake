e7aeb21e35ad032f0bcb777ba475ef9cec1c6ed9
vagrant <akzouksia@icloud.com>
2020-11-10 11:17:40 +0900

    [Add]admin customer_index_view

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,20 @@
 class CustomersController < ApplicationController
+
+	def index
+		@customers = Customer.all
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	private
+	def customer_params
+		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :phone_number, :address, :is_deleted)
+	end
 end
